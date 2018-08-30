5a31a6f6919cc0cae15ed58abe48a58e10f08ad7
Cho-Ching Lu <chochinlu@gmail.com>
2018-08-30 11:26:14 +0800

    Add create-react babel env settings.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
-const path = require('path');
+const path = require("path");
 const outputDir = path.join(__dirname, "build/");
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: './src/Index.bs.js',
-  mode: isProd ? 'production' : 'development',
+  entry: "./src/Index.bs.js",
+  mode: isProd ? "production" : "development",
+  module: {
+    rules: [
+      {
+        test: /\.react.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
   output: {
     path: outputDir,
     publicPath: outputDir,
-    filename: 'Index.js',
-  },
+    filename: "Index.js"
+  }
 };
